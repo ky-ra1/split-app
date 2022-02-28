764452ab5965a1db761584bcb077e92a59043fd0
Karishma Daya <kdaya001@gmail.com>
2022-02-28 22:50:58 +1100

    add controllers for payments

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const paymentEventsController = require('./controllers/payments_event');
 const usersController = require('./controllers/users');
+const paymentsController = require('./controllers/payments')
 
 // const db = require('./database/db');
 
@@ -16,6 +17,7 @@ app.use(express.static('client'));
 
 app.use("/api/paymentsEvent/", paymentEventsController);
 app.use('/api/users/', usersController);
+app.use('/api/payments', paymentsController)
 
 app.listen(port, () => {
     console.log(`server listening on port: ${port}`);
