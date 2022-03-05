ec2119425fffceb15c1d563e9359732bb3a58a6f
May <pakeeranam@gmail.com>
2022-03-05 16:55:07 +1100

    finish nav bar but need to style

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -11,11 +11,11 @@ function renderHeader(session = {}) {
                 aria-controls="sidebar_left"
             >
                 <img
-                    src="https://www.bylde.com/assets/img/bylde-logo-small.webp?v=B01234567"
-                    style="width: 30px"
+                    src="https://cdn.dribbble.com/users/2763870/screenshots/6940332/s_split_dribbble_grid.png"
                     data-bs-target="#sidebar_left"
                     data-bs-toggle="offcanvas"
                     class="img-fluid hlogo rounded"
+                    width="50"
                 />
             </a>
             <a
@@ -74,10 +74,62 @@ function renderHeader(session = {}) {
         });
     } else {
         header.innerHTML = `       
-        <ul id="navlist">
-            <li onClick="renderSignupForm()">Sign up</li> 
-            <li onClick="renderLoginForm()">Login</li> 
-        </ul>
+         <div class="w-100 border fixed-top py-2">
+            <a
+                class="btn float-start d-inline offcanvas_btn"
+                href="#sidebar_left"
+                role="button"
+                aria-controls="sidebar_left"
+            >
+                <img
+                    src="https://cdn.dribbble.com/users/2763870/screenshots/6940332/s_split_dribbble_grid.png"
+                    data-bs-target="#sidebar_left"
+                    data-bs-toggle="offcanvas"
+                    class="img-fluid hlogo rounded"
+                    width="50"
+                />
+            </a>
+            <a
+                class="btn float-end d-inline"
+                data-bs-toggle="offcanvas"
+                href="#sidebar_right"
+                role="button"
+                aria-controls="sidebar_right"
+            >
+                Login
+            </a>
+            <div class="clearfix"></div>
+        </div>
+        <div
+            class="offcanvas offcanvas-start"
+            tabindex="-1"
+            id="sidebar_left"
+            aria-labelledby="offcanvasExampleLabel"
+        >
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">
+                    Split App
+                </h5>
+                <button
+                    type="button"
+                    class="btn-close text-reset"
+                    data-bs-dismiss="offcanvas"
+                    aria-label="Close"
+                ></button>
+            </div>
+            <div class="offcanvas-body">
+                <div>Split is awesome.</div>
+                <ul id="navlist" class="nav flex-column">
+                     <li onClick="renderSignupForm()" class="nav-item">
+                        <a class="nav-link" href="#">Sign Up</a>
+                    </li>
+                    <li onClick="renderLoginForm()" class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div id="test login"></div>
 
     `;
     }
