8ba325ca9f0e0521e48472cad13b92583806c0e8
May <pakeeranam@gmail.com>
2022-03-05 16:21:36 +1100

    add nav bar

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -3,11 +3,66 @@ function renderHeader(session = {}) {
 
     if (session.email) {
         header.innerHTML = `       
-        <ul id="navlist">
-            <li id="main-page">Home</li> 
-            <li id="payment-event">Create Payment Event</li>
-            <li onClick="logout()">Logout</li> 
-        </ul>
+        <div class="w-100 border fixed-top py-2">
+            <a
+                class="btn float-start d-inline offcanvas_btn"
+                href="#sidebar_left"
+                role="button"
+                aria-controls="sidebar_left"
+            >
+                <img
+                    src="https://www.bylde.com/assets/img/bylde-logo-small.webp?v=B01234567"
+                    style="width: 30px"
+                    data-bs-target="#sidebar_left"
+                    data-bs-toggle="offcanvas"
+                    class="img-fluid hlogo rounded"
+                />
+            </a>
+            <a
+                class="btn float-end d-inline"
+                data-bs-toggle="offcanvas"
+                href="#sidebar_right"
+                role="button"
+                aria-controls="sidebar_right"
+            >
+                Login
+            </a>
+            <div class="clearfix"></div>
+        </div>
+        <div
+            class="offcanvas offcanvas-start"
+            tabindex="-1"
+            id="sidebar_left"
+            aria-labelledby="offcanvasExampleLabel"
+        >
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">
+                    Split App
+                </h5>
+                <button
+                    type="button"
+                    class="btn-close text-reset"
+                    data-bs-dismiss="offcanvas"
+                    aria-label="Close"
+                ></button>
+            </div>
+            <div class="offcanvas-body">
+                <div>Split is awesome.</div>
+                <ul id="navlist" class="nav flex-column">
+                    <li id="main-page" class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li id="payment-event" class="nav-item">
+                        <a class="nav-link" href="#">Create Payment Event</a>
+                    </li>
+                    <li onClick="logout()" class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div id="test login"></div>
+
     `;
         document
             .querySelector('#payment-event')
