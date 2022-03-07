be2769cd200440ebbc41f4e4d303360dafe6f9f4
Karishma Daya <kdaya001@gmail.com>
2022-03-07 23:37:46 +1100

    update UI and logic for users in payment events

--- client/js/components/payments_event/list.js
+++ client/js/components/payments_event/list.js
@@ -4,30 +4,88 @@ function renderPaymentEvent(event_id) {
     axios
         .get(`/api/paymentsEvent/getByEventId/${event_id}`)
         .then((response) => {
-            console.log(`asdas`, response.data);
-            page.innerHTML = `
+            axios
+                .get(`/api/users/`)
+                .then((usersResponse) => {
+                    const users = {};
+
+                    usersResponse.data.forEach((user) => {
+                        users[user.id] = {
+                            id: user.id,
+                            username: user.username,
+                        };
+                    });
+
+                    page.innerHTML = `
                 <div>
                     <h1>${response.data.rows[0].event_name}</h1>
                     <p>Total Amount: ${response.data.rows[0].total_amount}</p>
                 `;
-            response.data.rows.forEach((item) => {
-                page.innerHTML += `
-                    <div>
-                        <p>Created by: ${item.event_creator_id}</p>
-                        <p>User: ${item.user_id}</p>
-                        <p>Amount: ${item.amount}</p>
-                        <p>Due Date: ${moment(item.due_date).format(
+
+                    let table = document.createElement('table');
+                    let thead = document.createElement('thead');
+                    let tbody = document.createElement('tbody');
+                    tbody.setAttribute('id', 'paymentEvent');
+
+                    table.appendChild(thead);
+                    table.appendChild(tbody);
+
+                    page.appendChild(table);
+
+                    let row_heading = document.createElement('tr');
+                    let heading_1 = document.createElement('th');
+                    heading_1.innerHTML = 'Created By';
+                    let heading_2 = document.createElement('th');
+                    heading_2.innerHTML = 'User';
+                    let heading_3 = document.createElement('th');
+                    heading_3.innerHTML = 'Amount';
+                    let heading_4 = document.createElement('th');
+                    heading_4.innerHTML = 'Due Date';
+                    let heading_5 = document.createElement('th');
+                    heading_5.innerHTML = 'Status';
+
+                    row_heading.appendChild(heading_1);
+                    row_heading.appendChild(heading_2);
+                    row_heading.appendChild(heading_3);
+                    row_heading.appendChild(heading_4);
+                    row_heading.appendChild(heading_5);
+                    thead.appendChild(row_heading);
+
+                    response.data.rows.forEach((item) => {
+                        let row = document.createElement('tr');
+                        let row_data_1 = document.createElement('td');
+                        row_data_1.innerHTML = `${
+                            users[item.event_creator_id].username
+                        }`;
+                        let row_data_2 = document.createElement('td');
+                        row_data_2.innerHTML = `${
+                            users[item.user_id].username
+                        }`;
+                        let row_data_3 = document.createElement('td');
+                        row_data_3.innerHTML = `${item.amount}`;
+                        let row_data_4 = document.createElement('td');
+                        row_data_4.innerHTML = `${moment(item.due_date).format(
                             'D MMMM YYYY'
-                        )}</p>
-                        <p>Paid: ${
+                        )}`;
+                        let row_data_5 = document.createElement('td');
+                        row_data_5.innerHTML = `${
                             item.paid_status && item.received_status
                                 ? 'paid'
                                 : 'not paid'
-                        }</p>
-                    </div>
-                `;
-            });
-            page.innerHTML += `</div>`;
+                        }`;
+
+                        row.appendChild(row_data_1);
+                        row.appendChild(row_data_2);
+                        row.appendChild(row_data_3);
+                        row.appendChild(row_data_4);
+                        row.appendChild(row_data_5);
+                        tbody.appendChild(row);
+                    });
+                })
+                .catch((error) => {
+                    clearErrors();
+                    displayError(error);
+                });
         })
         .catch((error) => {
             clearErrors();
