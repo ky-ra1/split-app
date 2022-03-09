7e23acc7c88547202dfde1faa36cb5ceea1da6d8
May <pakeeranam@gmail.com>
2022-03-09 14:57:19 +1100

    add menu button on the right

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -81,9 +81,15 @@ function renderHeader(session = {}) {
                     width="50"
                 />LIT
             </a>
-           
+           <a class="btn float-end d-inline offcanvas_btn" href="#sidebar_left" role="button" aria-controls="sidebar_left">
+                <i class="material-icons" style="font-size:40px;color:black" data-bs-target="#sidebar_left"
+                    data-bs-toggle="offcanvas"
+                    class="img-fluid hlogo rounded"
+                    width="50">menu</i>
+            </a>
             <div class="clearfix"></div>
         </div>
+        
         <div
             class="offcanvas offcanvas-start"
             tabindex="-1"
