878b7684eb3ad5324304c69244de4b5d2aaad85c
May <pakeeranam@gmail.com>
2022-03-09 20:22:38 +1100

    edit nav bar

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -10,14 +10,14 @@ function renderHeader(session = {}) {
                 role="button"
                 aria-controls="sidebar_left"
             >
-                <img
+                <img 
                     src="https://static.thenounproject.com/png/180195-200.png"
-                    data-bs-target="#sidebar_left"
-                    data-bs-toggle="offcanvas"
+                    class="main-page"
+                    
                     class="img-fluid hlogo rounded"
                     width="50"
                 />PLIT
-            </a>
+            
             <a class="btn float-end d-inline offcanvas_btn" href="#sidebar_left" role="button" aria-controls="sidebar_left">
                 <i class="material-icons" style="font-size:40px;color:black" data-bs-target="#sidebar_left"
                     data-bs-toggle="offcanvas"
@@ -28,7 +28,7 @@ function renderHeader(session = {}) {
             <div class="clearfix"></div>
         </div>
         <div
-            class="offcanvas offcanvas-start"
+            class="offcanvas offcanvas-end"
             tabindex="-1"
             id="sidebar_left"
             aria-labelledby="offcanvasExampleLabel"
@@ -47,7 +47,7 @@ function renderHeader(session = {}) {
             <div class="offcanvas-body">
                 <div>Hello <span style="font-weight: bold; color: #d500f9">${session.username}</span>!</div>
                 <ul id="navlist" class="nav flex-column">
-                    <li id="main-page" class="nav-item">
+                    <li class="main-page" class="nav-item">
                         <a class="nav-link active" href="#">Home</a>
                     </li>
                     <li id="payment-event" class="nav-item">
@@ -67,7 +67,7 @@ function renderHeader(session = {}) {
             .addEventListener('click', () => {
                 renderCreatePaymentEventList(session);
             });
-        document.querySelector('#main-page').addEventListener('click', () => {
+        document.querySelector('.main-page').addEventListener('click', () => {
             mainPageElement(session);
         });
     } else {
@@ -79,10 +79,10 @@ function renderHeader(session = {}) {
                 role="button"
                 aria-controls="sidebar_left"
             >
-                <img
+                 <img 
                     src="https://static.thenounproject.com/png/180195-200.png"
-                    data-bs-target="#sidebar_left"
-                    data-bs-toggle="offcanvas"
+                    class="main-page"
+                    
                     class="img-fluid hlogo rounded"
                     width="50"
                 />PLIT
@@ -97,7 +97,7 @@ function renderHeader(session = {}) {
         </div>
         
         <div
-            class="offcanvas offcanvas-start"
+            class="offcanvas offcanvas-end"
             tabindex="-1"
             id="sidebar_left"
             aria-labelledby="offcanvasExampleLabel"
